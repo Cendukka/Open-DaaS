9f3b31463ecc14e6bf98c66069b46466477c49b3
eemeli.ranta <eemeli.ranta@git.thefirma.fi>
2019-06-06 17:27:03 +0300

    Made a different kind of ordering to Sorted page

--- resources/views/pages/company/sorted.blade.php
+++ resources/views/pages/company/sorted.blade.php
@@ -9,13 +9,26 @@
                 <div class="panel-heading">
                     @php
                         $from_date = isset($_GET['from_date']) ? $_GET['from_date'] : date('Y-m-d', strtotime("-12 months", strtotime(date('Y-m-d'))));
-                        $to_date = isset($_GET['to_date']) ? $_GET['to_date'] : date('Y-m-d') ;
+                        $to_date = isset($_GET['to_date']) ? $_GET['to_date'] : date('Y-m-d');
+                        $sort_by = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'id';
+                        $order_by = isset($_GET['order_by']) ? $_GET['order_by'] : 'asc';
                     @endphp
-                    <form action={{"/companies/".$company->company_id."/sorting"}}>
+                    <form action={{"/companies/".$company->company_id."/sorted"}}>
                         From date:<br>
                         <input type="text" name="from_date" value={{$from_date}}><br>
                         To date:<br>
-                        <input type="text" name="to_date" value={{$to_date}}>
+                        <input type="text" name="to_date" value={{$to_date}}><br>
+                        <select name="sort_by">
+                            <option value="id" {{$sort_by=='id' ? 'selected="selected"' : ''}}>ID</option>
+                            <option value="date" {{$sort_by=='date' ? 'selected="selected"' : ''}}>Date</option>
+                            <option value="weight" {{$sort_by=='weight' ? 'selected="selected"' : ''}}>Weight</option>
+                            <option value="material" {{$sort_by=='material' ? 'selected="selected"' : ''}}>Material</option>
+                            <option value="user" {{$sort_by=='user' ? 'selected="selected"' : ''}}>User</option>
+                        </select>
+                        <select name="order_by">
+                            <option value="asc" {{$order_by=='asc' ? 'selected="selected"' : ''}}>Ascending</option>
+                            <option value="desc" {{$order_by=='desc' ? 'selected="selected"' : ''}}>Descending</option>
+                        </select><br>
                         <input type="submit" value="Tarkenna Hakua">
                     </form>
                     <table>
@@ -36,6 +49,11 @@
                                 array_push($microlocation_ids, $microlocation->microlocation_id);
                             }
 
+                            $sort_by = ($sort_by == 'id' ? 'inventory_receipt.receipt_to_microlocation_id' : (
+                                        $sort_by == 'date' ? 'pre_sorting.pre_sorting_date' : (
+                                        $sort_by == 'weight' ? 'pre_sorting.pre_sorting_weight' : (
+                                        $sort_by == 'material' ? 'presorted_material.presorted_material_name' : 'pre_sorting.pre_sorting_user_id'
+                                        ))));
 
                             $inventory = DB::table('pre_sorting')
                                         ->join('inventory_receipt','pre_sorting.pre_sorting_inventory_receipt_id','=','inventory_receipt.inventory_receipt_id')
@@ -43,8 +61,7 @@
                                         ->whereIn('inventory_receipt.receipt_to_microlocation_id', $microlocation_ids)
                                         ->where('pre_sorting.pre_sorting_status_id','=','1')
                                         ->whereBetween('receipt_date', [$from_date, $to_date])
-                                        ->orderBy('inventory_receipt.receipt_to_microlocation_id')
-                                        ->orderBy('pre_sorting.pre_sorting_date')
+                                        ->orderBy($sort_by,$order_by)
                                         ->get();
                         @endphp
                         @foreach ($inventory as $inv_item)

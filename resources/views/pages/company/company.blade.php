53fd8c267dd2fce42fbe36005e4a47b09096ff6b
eemeli.ranta <eemeli.ranta@git.thefirma.fi>
2019-06-10 09:58:03 +0300

    Updated warehouse page

--- resources/views/pages/company/company.blade.php
+++ resources/views/pages/company/company.blade.php
@@ -10,8 +10,14 @@
                     <table>
                         <tr>
                             <th>ID</th>
-                            @foreach (DB::table('textile_inventory')->distinct()->select('fraction')->get() as $fraction)
-                                <th>{{title_case($fraction->fraction)}}</th>
+                            @php
+                                $material_names = DB::table('inventory')->distinct()
+                                                    ->select('material_names.material_id','material_names.material_name')
+                                                    ->join('material_names', 'inventory.inventory_material_id','=','material_names.material_id')
+                                                    ->get();
+                            @endphp
+                            @foreach ($material_names as $material)
+                                <th>{{title_case($material->material_name)}}</th>
                             @endforeach
                         </tr>
                         @php
@@ -22,23 +28,22 @@
                             $inventory = [];
                             foreach($microlocations as $microlocation){
                                 array_push($inventory, $microlocation->microlocation_id =
-                                    DB::table('textile_inventory')
-                                        ->where('textile_inventory.textile_microlocation_id', '=', $microlocation->microlocation_id)
-                                        ->orderBy('textile_inventory.textile_microlocation_id')
-                                        ->orderBy('textile_inventory.fraction')
+                                    DB::table('inventory')
+                                        ->where('inventory.inventory_microlocation_id', '=', $microlocation->microlocation_id)
+                                        ->orderBy('inventory.inventory_microlocation_id')
+                                        ->orderBy('inventory.inventory_material_id')
                                         ->get());
                             }
                         @endphp
                         @foreach ($inventory as $inv_item)
                             <tr>
-                                <td>{{title_case($inv_item[0]->textile_microlocation_id)}}</td>
+                                <td>{{title_case($inv_item[0]->inventory_microlocation_id)}}</td>
                                 @foreach ($inv_item as $inv)
-                                    <td>{{title_case($inv->textile_weight)}}</td>
+                                    <td>{{title_case($inv->inventory_weight)}}</td>
                                 @endforeach
                             </tr>
                         @endforeach
                     </table>
-
                 </div>
             </div>
         </div>

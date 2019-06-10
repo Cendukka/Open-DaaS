a254fb34ec5c269a67b9aeb20ee05b804430140e
eemeli.ranta <eemeli.ranta@git.thefirma.fi>
2019-06-10 13:22:52 +0300

    Fixed a bug in sortby

--- resources/views/pages/company/sorting.blade.php
+++ resources/views/pages/company/sorting.blade.php
@@ -11,28 +11,28 @@
                         $location = isset($_GET['location']) ? $_GET['location'] : '';
                         $from_date = isset($_GET['from_date']) ? date("Y-m-d", strtotime($_GET['from_date'])) : date('Y-m-d', strtotime("-12 months", strtotime(date('Y-m-d'))));
                         $to_date = isset($_GET['to_date']) ? date("Y-m-d", strtotime($_GET['to_date'])) : date('Y-m-d');
-                        $orderBy = (isset($_GET['order']) && $_GET['order'] == 'desc' ? 'DESC' : 'ASC');
+                        $order_by = (isset($_GET['order']) && $_GET['order'] == 'desc' ? 'DESC' : 'ASC');
 
-                        $get_string = 'location='.$location.'from_date='.$from_date.'&to_date='.$to_date;
+                        $get_string = 'location='.$location.'&from_date='.$from_date.'&to_date='.$to_date;
 
-                        $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
+                        $sort_by = 'inventory_receipt.receipt_to_microlocation_id';
 
-                        if(isset($_GET['sortBy'])){
-                            switch ($_GET['sortBy']){
+                        if(isset($_GET['sort_by'])){
+                            switch ($_GET['sort_by']){
                             case 'id':
-                                $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
+                                $sort_by = 'inventory_receipt.receipt_to_microlocation_id';
                                 break;
                             case 'date':
-                                $sortBy = 'pre_sorting.pre_sorting_date';
+                                $sort_by = 'pre_sorting.pre_sorting_date';
                                 break;
                             case 'weight':
-                                $sortBy = 'pre_sorting.pre_sorting_weight';
+                                $sort_by = 'pre_sorting.pre_sorting_weight';
                                 break;
                             case 'material':
-                                $sortBy = 'presorted_material.presorted_material_name';
+                                $sort_by = 'presorted_material.presorted_material_name';
                                 break;
                             case 'user':
-                                $sortBy = 'pre_sorting.pre_sorting_user_id';
+                                $sort_by = 'users.last_name';
                             }
                         }
                     @endphp
@@ -47,11 +47,11 @@
                     </form>
                     <table>
                         <tr>
-                            <th><a href="?{{$get_string}}&sortBy=id&order={{$orderBy == 'ASC' && $sortBy == 'inventory_receipt.receipt_to_microlocation_id' ? 'desc' : 'asc'}}">Location ID</a></th>
-                            <th><a href="?{{$get_string}}&sortBy=date&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_date' ? 'desc' : 'asc'}}">Date</a></th>
-                            <th><a href="?{{$get_string}}&sortBy=weight&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_weight' ? 'desc' : 'asc'}}">Weight (KG)</a></th>
-                            <th><a href="?{{$get_string}}&sortBy=material&order={{$orderBy == 'ASC' && $sortBy == 'presorted_material.presorted_material_name' ? 'desc' : 'asc'}}">Material</a></th>
-                            <th><a href="?{{$get_string}}&sortBy=user&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_user_id' ? 'desc' : 'asc'}}">User ID</a></th>
+                            <th><a href="?{{$get_string}}&sort_by=id&order={{$order_by == 'ASC' && $sort_by == 'inventory_receipt.receipt_to_microlocation_id' ? 'desc' : 'asc'}}">Location ID</a></th>
+                            <th><a href="?{{$get_string}}&sort_by=date&order={{$order_by == 'ASC' && $sort_by == 'pre_sorting.pre_sorting_date' ? 'desc' : 'asc'}}">Date</a></th>
+                            <th><a href="?{{$get_string}}&sort_by=weight&order={{$order_by == 'ASC' && $sort_by == 'pre_sorting.pre_sorting_weight' ? 'desc' : 'asc'}}">Weight (KG)</a></th>
+                            <th><a href="?{{$get_string}}&sort_by=material&order={{$order_by == 'ASC' && $sort_by == 'presorted_material.presorted_material_name' ? 'desc' : 'asc'}}">Material</a></th>
+                            <th><a href="?{{$get_string}}&sort_by=user&order={{$order_by == 'ASC' && $sort_by == 'users.last_name' ? 'desc' : 'asc'}}">User</a></th>
                         </tr>
                         @php
                             $microlocations = DB::table('microlocations')
@@ -66,13 +66,14 @@
                             $inventory = DB::table('pre_sorting')
                                         ->join('inventory_receipt','pre_sorting.pre_sorting_receipt_id','=','inventory_receipt.receipt_id')
                                         ->join('presorted_material','pre_sorting.presorted_material_id','=','presorted_material.presorted_material_id')
+                                        ->join('users','pre_sorting.pre_sorting_user_id','=','users.user_id')
                                         ->whereIn('inventory_receipt.receipt_to_microlocation_id', $microlocation_ids)
                                         ->where('pre_sorting.pre_sorting_status_id','=','2')
                                         ->when($location, function ($query, $location) {
                                             return $query->where('inventory_receipt.receipt_to_microlocation_id', '=', $location);
                                         })
                                         ->whereBetween('receipt_date', [$from_date, $to_date])
-                                        ->orderBy($sortBy,$orderBy)
+                                        ->orderBy($sort_by,$order_by)
                                         ->get();
 
                         @endphp
@@ -82,7 +83,7 @@
                                 <td>{{title_case($inv_item->pre_sorting_date)}}</td>
                                 <td>{{title_case($inv_item->pre_sorting_weight)}}</td>
                                 <td>{{title_case($inv_item->presorted_material_name)}}</td>
-                                <td>{{title_case($inv_item->pre_sorting_user_id)}}</td>
+                                <td>{{title_case($inv_item->last_name)}} {{title_case($inv_item->first_name)}}</td>
                             </tr>
                         @endforeach
                     </table>

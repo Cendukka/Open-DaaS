c4b7cbdb8d98f550cf59ad14511e5b1ad94b49be
eemeli.ranta <eemeli.ranta@git.thefirma.fi>
2019-06-10 11:19:34 +0300

    Added filter by microlocation id

--- resources/views/pages/company/sorting.blade.php
+++ resources/views/pages/company/sorting.blade.php
@@ -8,25 +8,37 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     @php
+                        $location = isset($_GET['location']) ? $_GET['location'] : '';
                         $from_date = isset($_GET['from_date']) ? date("Y-m-d", strtotime($_GET['from_date'])) : date('Y-m-d', strtotime("-12 months", strtotime(date('Y-m-d'))));
                         $to_date = isset($_GET['to_date']) ? date("Y-m-d", strtotime($_GET['to_date'])) : date('Y-m-d');
                         $orderBy = (isset($_GET['order']) && $_GET['order'] == 'desc' ? 'DESC' : 'ASC');
 
+                        $get_string = 'location='.$location.'from_date='.$from_date.'&to_date='.$to_date;
+
                         $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
-                            if(isset($_GET['sortBy'])){
-                                switch ($_GET['sortBy']){
-                                case 'id':
-                                    $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
-                                    break;
-                                case 'date':
-                                    $sortBy = 'pre_sorting.pre_sorting_date';
-                                    break;
-                                case 'weight':
-                                    $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
-                                }
+
+                        if(isset($_GET['sortBy'])){
+                            switch ($_GET['sortBy']){
+                            case 'id':
+                                $sortBy = 'inventory_receipt.receipt_to_microlocation_id';
+                                break;
+                            case 'date':
+                                $sortBy = 'pre_sorting.pre_sorting_date';
+                                break;
+                            case 'weight':
+                                $sortBy = 'pre_sorting.pre_sorting_weight';
+                                break;
+                            case 'material':
+                                $sortBy = 'presorted_material.presorted_material_name';
+                                break;
+                            case 'user':
+                                $sortBy = 'pre_sorting.pre_sorting_user_id';
                             }
+                        }
                     @endphp
                     <form action={{"/companies/".$company->company_id."/sorting"}}>
+                        Microlocation ID:<br>
+                        <input type="text" name="location" value={{$location}}><br>
                         From date:<br>
                         <input type="text" name="from_date" value={{$from_date}}><br>
                         To date:<br>
@@ -35,11 +47,11 @@
                     </form>
                     <table>
                         <tr>
-                            <th><a href="?sortBy=id&order={{$orderBy == 'ASC' && $sortBy == 'inventory_receipt.receipt_to_microlocation_id' ? 'desc' : 'asc'}}">Location ID</a></th>
-                            <th><a href="?sortBy=date&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_date' ? 'desc' : 'asc'}}">Date</a></th>
-                            <th>Weight (KG)</th>
-                            <th>Material</th>
-                            <th>User ID</th>
+                            <th><a href="?{{$get_string}}&sortBy=id&order={{$orderBy == 'ASC' && $sortBy == 'inventory_receipt.receipt_to_microlocation_id' ? 'desc' : 'asc'}}">Location ID</a></th>
+                            <th><a href="?{{$get_string}}&sortBy=date&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_date' ? 'desc' : 'asc'}}">Date</a></th>
+                            <th><a href="?{{$get_string}}&sortBy=weight&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_weight' ? 'desc' : 'asc'}}">Weight (KG)</a></th>
+                            <th><a href="?{{$get_string}}&sortBy=material&order={{$orderBy == 'ASC' && $sortBy == 'presorted_material.presorted_material_name' ? 'desc' : 'asc'}}">Material</a></th>
+                            <th><a href="?{{$get_string}}&sortBy=user&order={{$orderBy == 'ASC' && $sortBy == 'pre_sorting.pre_sorting_user_id' ? 'desc' : 'asc'}}">User ID</a></th>
                         </tr>
                         @php
                             $microlocations = DB::table('microlocations')
@@ -51,14 +63,14 @@
                                 array_push($microlocation_ids, $microlocation->microlocation_id);
                             }
 
-
-
-
                             $inventory = DB::table('pre_sorting')
                                         ->join('inventory_receipt','pre_sorting.pre_sorting_receipt_id','=','inventory_receipt.receipt_id')
                                         ->join('presorted_material','pre_sorting.presorted_material_id','=','presorted_material.presorted_material_id')
                                         ->whereIn('inventory_receipt.receipt_to_microlocation_id', $microlocation_ids)
                                         ->where('pre_sorting.pre_sorting_status_id','=','2')
+                                        ->when($location, function ($query, $location) {
+                                            return $query->where('inventory_receipt.receipt_to_microlocation_id', '=', $location);
+                                        })
                                         ->whereBetween('receipt_date', [$from_date, $to_date])
                                         ->orderBy($sortBy,$orderBy)
                                         ->get();

608db8f97ed5910721eddba3df48754a04472cb3
andreiskorik <47348551+andreiskorik@users.noreply.github.com>
2019-06-13 12:10:45 +0300

    New CSS and new code for Head and Header

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,14 @@
 <meta charset="utf-8">
-<meta name="description" content="">
-<!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>BigData sidebar using Bootstrap 3</title>
+
+         <!-- Bootstrap CSS CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Our Custom CSS -->
+        <link rel="stylesheet" href="<?php echo asset('css/sidebar.css')?>" type="text/css"> 
+        
 
 <style>
     * {

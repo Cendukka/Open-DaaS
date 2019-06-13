4e184a2090016258926dfdb901b9094fac209f2f
andreiskorik <47348551+andreiskorik@users.noreply.github.com>
2019-06-13 16:02:05 +0300

    Bootsrap customization

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,38 +9,3 @@
         <!-- Our Custom CSS -->
         <link rel="stylesheet" href="<?php echo asset('css/sidebar.css')?>" type="text/css"> 
         
-
-<style>
-    * {
-        box-sizing: border-box;
-    }
-
-    /* Create two equal columns that floats next to each other */
-    .column {
-        float: left;
-        padding: 10px;
-        height: 300px; /* Should be removed. Only for demonstration */
-    }
-
-    /* Clear floats after the columns */
-    .row:after {
-        content: "";
-        display: table;
-        clear: both;
-    }
-
-    table, th, td {
-        border: 1px solid black;
-        text-align: left;
-    }
-
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
-
-    th {
-        height: 50px;
-    }
-
-</style>

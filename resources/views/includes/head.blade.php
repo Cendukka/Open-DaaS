c99634a5114e0095bc0f6de114f0c324ff112603
andreiskorik <47348551+andreiskorik@users.noreply.github.com>
2019-06-14 14:09:55 +0300

    Title update

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>BigData sidebar using Bootstrap 3</title>
+        <title>BigData Pilot (sidebar)</title>
 
          <!-- Bootstrap CSS CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">

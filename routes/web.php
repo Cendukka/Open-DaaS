301bb631b3237c69f992784263d357f1bfbea3cc
Prabin Pandey <stormrider6225@gmail.com>
2019-06-17 10:56:07 +0300

    changes from emeli's web file and auth route added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
 Route::get('/', function () {
 	return view('pages.home');
 });
@@ -19,16 +28,52 @@ Route::get('/manage', function () {
 	return view('pages.manage');
 });
 
-
-Route::resource('companies', 'company_controller', ['only' => [
-	'index', 'show'
-]]);
-
-Route::resource('companies', 'company_controller', ['except' => [
-	'create', 'store', 'update', 'destroy'
-]]);
+Route::get('ewc','ewc_controller@index');
+Route::get('ewc/search','ewc_controller@search');
 
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+# Report Pages
+Route::get('companies/{company}/warehouse', 'company_controller@warehouse_index');
+
+Route::get('companies/{company}/receipts', 'company_controller@receipts_index');
+
+Route::get('companies/{company}/pre', 'company_controller@pre_index');
+
+Route::get('companies/{company}/refined', 'company_controller@refined_index');
+
+Route::get('companies/{company}/issues', 'company_controller@issues_index');
+
+
+#Manage Pages
+Route::get('companies/{company}/manage', 'company_controller@manage_index');
+
+
+
+Route::resource('materials', 'materials_controller', ['only' => ['index', 'show', 'create', 'edit']]);
+Route::post('materials/materials-store', 'materials_controller@store');
+Route::post('materials/{material}/materials-update', 'materials_controller@update');
+Route::post('materials/{material}/materials-destroy', 'materials_controller@destroy');
+
+# Companies
+Route::resource('companies', 'company_controller', ['only' => ['index', 'show', 'create', 'edit']]);
+Route::post('companies/company-store', 'company_controller@store');
+Route::post('companies/{company}/company-update', 'company_controller@update');
+
+# Users
+Route::resource('companies/{company}/manage/users', 'user_controller', ['only' => ['index', 'show', 'create', 'edit']]);
+Route::post('companies/{company}/manage/users/users-store', 'user_controller@store');
+Route::post('companies/{company}/manage/users/{user}/users-update', 'user_controller@update');
+
+# Microlocations
+Route::resource('companies/{company}/manage/microlocations', 'microlocation_controller', ['only' => ['index', 'show', 'create', 'edit']]);
+Route::post('companies/{company}/manage/microlocations/microlocations-store', 'microlocation_controller@store');
+Route::post('companies/{company}/manage/microlocations/{microlocation}/microlocations-update', 'microlocation_controller@update');
+
+# Receipts
+Route::resource('companies/{company}/manage/receipts', 'receipt_controller', ['only' => ['index', 'show', 'create', 'edit']]);
+Route::post('companies/{company}/manage/receipts/receipts-store', 'receipt_controller@store');
+Route::post('companies/{company}/manage/receipts/{receipt}/receipts-update', 'receipt_controller@update');
+
+#Route::get('/','SearchController@index');
+#Route::get('/search','SearchController@search');

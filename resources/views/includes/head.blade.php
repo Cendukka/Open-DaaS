0b5dd5977b0831a8853895021100e4fdf8691828
andreiskorik <47348551+andreiskorik@users.noreply.github.com>
2019-06-17 15:01:51 +0300

    Brush up in the code and commits

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,8 +6,10 @@
 
          <!-- Bootstrap CSS CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
         <!-- Our Custom CSS -->
         <link rel="stylesheet" href="<?php echo asset('css/sidebar.css')?>" type="text/css"> 
-        <!-- Scrollbar Custom CSS -->
+        
+        <!-- Scrollbar Custom CSS (for fixed LeftSide Menu) -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
         
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,63 +1,77 @@
-        <div class="wrapper">
-            <!-- Sidebar Holder -->
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <a href="/"><h3>JALOSTUSLAITOS</h3>(BigData Pilot)</a>
+<div class="wrapper">
+    
+    <!-- Sidebar Holder -->
+    <nav id="sidebar">
+		
+		<div class="sidebar-header">
+        	<a href="/"><h3>JALOSTUSLAITOS</h3>(BigData Pilot)</a>
+		</div>
+
+		<ul class="list-unstyled components">
+
+			<li>
+				<a href="#">Oma toimipiste</a>
+				<a href="/companies/">Toimipisteet</a>
+                <a href="/ewc/">EWC Codes</a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Hallinnoi</a>
+                
+                	<ul class="collapse list-unstyled" id="pageSubmenu">
+                    	<li><a href="/manage/">K‰ytt‰j‰oikeudet</a></li>
+                        <li><a href="#">Kategoriat</a></li>
+                        <li><a href="#">Lis‰‰ uusi toimipiste</a></li>
+					</ul>
+			
+			</li>
+
+		</ul>
+
+        <ul class="list-unstyled CTAs">
+        	<li><a href="/" class="back">Back</a></li>
+        </ul>
+	
+	</nav>
+
+    <!-- Page Content Holder -->
+    <div id="content">
+
+		<nav class="navbar navbar-default">
+        	<div class="container-fluid">
+            	
+            	<div class="navbar-header">
+                	
+                	<button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn toggle">
+                    	<i class="glyphicon glyphicon-align-left"></i>
+                        	<span>Toggle Sidebar</span>
+                    </button>
+                        
                 </div>
 
-                <ul class="list-unstyled components">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                	
+                	<ul class="nav navbar-nav navbar-right">
+                    	<li><a href="#" class="info-btn">Yhteystiedot</a></li>
+                        <li><a href="#" class="btn btn-info btn-lg logout">KIRJAUDU ULOS</a></li>
+                    </ul>
+                        
+                </div>
+                    
+			</div>
 
-                    <li>
-                        <a href="#">Oma toimipiste</a>
-                        <a href="/companies/">Toimipisteet</a>
-                        <a href="/ewc/">EWC Codes</a>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Hallinnoi</a>
-                        <ul class="collapse list-unstyled" id="pageSubmenu">
-                            <li><a href="/manage/">K‰ytt‰j‰oikeudet</a></li>
-                            <li><a href="#">Kategoriat</a></li>
-                            <li><a href="#">Lis‰‰ uusi toimipiste</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-
-                <ul class="list-unstyled CTAs">
-                    <li><a href="/" class="back">Back</a></li>
-                </ul>
-            </nav>
-
-            <!-- Page Content Holder -->
-            <div id="content">
-
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-
-                        <div class="navbar-header">
-                            <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn toggle">
-                                <i class="glyphicon glyphicon-align-left"></i>
-                                <span>Toggle Sidebar</span>
-                            </button>
-                        </div>
-
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li><a href="#" class="info-btn">Yhteystiedot</a></li>
-                                <li><a href="#" class="btn btn-info btn-lg logout">KIRJAUDU ULOS</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+		</nav>
                 
-                   <div id="main" class="column">
-      				@yield('content')
-   					</div>
+		<!-- Main Page Content Holder -->
+		<div id="main" class="column">
+      		@yield('content')
+   		</div>
 
-                 <button onclick="topFunction()" class="btn btn-default btn-sm" id="toTop" title="Go to top"><span class="glyphicon glyphicon-arrow-up"></span> UP</button> 
+        <!-- Back To The Top Button -->
+        <button onclick="topFunction()" class="btn btn-default btn-sm" id="toTop" title="Go to top"><span class="glyphicon glyphicon-arrow-up"></span> UP</button> 
                 
-            </div>
-        </div>
+	</div>
 
-<!-- jQuery CDN -->
+</div>
+
+<!-- jQuery CDN for LeftSide Menu-->
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <!-- Bootstrap Js CDN -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -78,8 +92,10 @@
             });
         </script>
 
+<!-- Script for Back To The Top Button -->
 <script>
-// When the user scrolls down 30px from the top of the document, show the button
+
+<!-- When the user scrolls down 30px from the top of the document, show the button -->
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -90,7 +106,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+<!-- When the user clicks on the button, scroll to the top of the document -->
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;

03e710224e52118d49b5a29073f5fb8cf3c37bab
andreiskorik <47348551+andreiskorik@users.noreply.github.com>
2019-06-17 14:37:27 +0300

    Change in comments

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -79,7 +79,7 @@
         </script>
 
 <script>
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 30px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {

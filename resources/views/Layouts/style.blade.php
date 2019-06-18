7048c9ff3e88bfeec1e4db30f5e466ef9df0901b
meAjit <38285723+meAjit@users.noreply.github.com>
2019-06-18 15:18:36 +0300

    comment in my old version pages

--- resources/views/Layouts/style.blade.php
+++ resources/views/Layouts/style.blade.php
@@ -1,4 +1,16 @@
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+   <!-- --------------------------------------
+
+    this is my old version style include
+
+     -----------------------------------------   -->
+
+
+
+
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>

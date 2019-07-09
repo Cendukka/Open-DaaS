62d1267b91272049aa5b811b32683eebaaf9d873
meAjit <38285723+meAjit@users.noreply.github.com>
2019-07-09 10:15:07 +0300

    css changes

--- resources/views/pages/companies.blade.php
+++ resources/views/pages/companies.blade.php
@@ -6,8 +6,8 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     @foreach (DB::table('company')->get() as $company)
-                        <div class="panel-heading">
-                            <a href="{{url('/companies/'.$company->company_id)}}">{{title_case($company->company_name)}}</a>
+                        <div class="panel-heading"> 
+                            <a class="textMark" href="{{url('/companies/'.$company->company_id)}}">{{title_case($company->company_name)}}</a>
                         </div>
                     @endforeach
                         <br>

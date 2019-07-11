9b9c50a2218e8814269a70fb3c4bf1086236d9c7
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-07-11 15:49:06 +0300

    Update receipts.blade.php

--- resources/views/pages/company/receipts.blade.php
+++ resources/views/pages/company/receipts.blade.php
@@ -52,52 +52,7 @@
                 <form action="{{url('companies/'.$company->company_id.'/manage/receipts/create')}}">
                     <button type="submit" class="btn btn-secondary">+ Lisää saapunut lähetys</button>
                 </form>
-{{--                    <table>--}}
-{{--                        <tr>--}}
-{{--                            <th>ID</th>--}}
-{{--                            <th>From ID</th>--}}
-{{--                            <th>Date</th>--}}
-{{--                            <th>Material</th>--}}
-{{--                            <th>Weight (kg)</th>--}}
-{{--                            <th>Distance (km)</th>--}}
-{{--                            <th>EWC Code</th>--}}
-{{--                        </tr>--}}
-{{--                        @foreach ($inventory as $inv_item)--}}
-{{--                            <tr>--}}
-{{--                                @php--}}
-{{--                                    $fromid =  ($inv_item->from_company_id ? 'Company '.$inv_item->from_company_id :--}}
-{{--                                               ($inv_item->from_community_id ? 'Community '.$inv_item->from_community_id :--}}
-{{--                                               ($inv_item->from_supplier_id ? 'Supplier '.$inv_item->from_supplier_id :--}}
-{{--                                                'Microlocation '.$inv_item->receipt_from_microlocation_id)));--}}
-{{--                                @endphp--}}
-{{--                                <td>{{title_case($inv_item->receipt_to_microlocation_id)}}</td>--}}
-{{--                                <td>{{$fromid}}</td>--}}
-{{--                                <td>{{title_case($inv_item->receipt_date)}}</td>--}}
-{{--                                <td>{{title_case($inv_item->material_name)}}</td>--}}
-{{--                                <td>{{title_case($inv_item->receipt_weight)}}</td>--}}
-{{--                                <td>{{title_case($inv_item->distance_km)}}</td>--}}
-{{--                                <td>{{title_case($inv_item->receipt_ewc_code)}}</td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
-{{--                        @php--}}
-{{--                            $totalWeight = 0;--}}
-{{--                            foreach ($inventory as $inv_item){--}}
-{{--                                $totalWeight += $inv_item->receipt_weight;--}}
-{{--                            }--}}
-{{--                        @endphp--}}
-{{--                        <tr>--}}
-{{--                            <td></td>--}}
-{{--                            <td></td>--}}
-{{--                            <td></td>--}}
-{{--                            <td>Total:</td>--}}
-{{--                            <td>{{$totalWeight}}</td>--}}
-{{--                            <td></td>--}}
-{{--                            <td></td>--}}
-{{--                        </tr>--}}
-{{--                    </table>--}}
-{{--                @else--}}
-{{--                    <h4>Unable to find any records</h4>--}}
-{{--                @endif--}}
+
             </div>
         </div>
     </div>

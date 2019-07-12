0463fef72935de58cd0e586d7fad7189b9bd9f10
meAjit <38285723+meAjit@users.noreply.github.com>
2019-07-12 10:35:45 +0300

    home page for the super admin

--- resources/views/includes/microlocation_header.blade.php
+++ resources/views/includes/microlocation_header.blade.php
@@ -65,7 +65,8 @@
 
                     <ul class="nav navbar-nav navbar-right">
 
-                    <a href="/home" class="btn btn-info btn-lg logout">Etusivu</a>
+                        
+                        <a href="/home" class="btn btn-info btn-lg logout">Etusivu</a>
                         <a href="/" class="btn btn-info btn-lg logout">Julkinen sivu</a>
                         @guest
                             <a href="{{ route('login') }}" class="btn btn-info btn-lg logout">KIRJAUDU SISÄÄN</a>

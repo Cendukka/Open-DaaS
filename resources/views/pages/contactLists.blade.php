890ec60fa1911871bfbd6434e0f734f7073377a1
meAjit <38285723+meAjit@users.noreply.github.com>
2019-07-15 14:28:02 +0300

    some contact added

--- resources/views/pages/contactLists.blade.php
+++ resources/views/pages/contactLists.blade.php
@@ -20,12 +20,11 @@
                             <th>Address</th>
                         </tr>
                         <tr>
-                            <td>Botniarosk </td>
+                            <td>Botniarosk</td>
                             <td>+358 40 594 5121</td>
                             <td></td>
                             <td> <a href="https://www.botniarosk.fi/" target="_blank">www.botniarosk.fi</a></td>
-                            <td>Raatihuoneenkatu 1 
-                                64101 Kristiinankaupunki</td>
+                            <td>Raatihuoneenkatu 164101 Kristiinankaupunki</td>
                         </tr>
                         <tr>
                             <td>Ekokymppi</td>
@@ -33,6 +32,62 @@
                             <td>info@ekokymppi.fi</td>
                             <td> <a href="https://www.ekokymppi.fi/" target="_blank">www.ekokymppi.fi</a></td>
                             <td>Viestitie 2, 87700 Kajaani</td>
+                        </tr>
+                        <tr>
+                            <td>Ab Ekorosk Oy</td>
+                            <td> (06) 781 4500</td>
+                            <td></td>
+                            <td> <a href="https://www.ekorosk.fi/" target="_blank">www.ekorosk.fi</a></td>
+                            <td>Launisaarentie 9068600 PIETARSAARI</td>
+                        </tr>
+                        <tr>
+                            <td>Etelä-Karjalan Jätehuolto Oy</td>
+                            <td>0108411818</td>
+                            <td>asiakaspalvelu@ekjh.fi</td>
+                            <td> <a href="https://www.ekjh.fi/" target="_blank">www.ekjh.fi</a></td>
+                            <td>Viestitie 2, 87700 Kajaani</td>
+                        </tr>
+                        <tr>
+                            <td>HSY</td>
+                            <td>09 1561 2110</td>
+                            <td></td>
+                            <td> <a href="https://www.hsy.fi/" target="_blank">www.hsy.fi</a></td>
+                            <td>Ilmalantori 1, 00240 Helsinki</td>
+                        </tr>
+                        <tr>
+                            <td>Jätekukko</td>
+                            <td>+358 17 368 0152 </td>
+                            <td>service@jatekukko.fi</td>
+                            <td> <a href="https://www.jatekukko.fi/" target="_blank">www.jatekukko.fi</a></td>
+                            <td>Microkatu 1, Kuopio</td>
+                        </tr>
+                        <tr>
+                            <td>Kiertokaari</td>
+                            <td> (08) 5584 0010</td>
+                            <td> jatehuolto@kiertokaari.fi</td>
+                            <td> <a href="https://kiertokaari.fi/" target="_blank">kiertokaari.fi</a></td>
+                            <td>Kiertokaari Oy Ruskonniityntie 1090620 Oulu</td>
+                        </tr>
+                        <tr>
+                            <td>Kiertokapula</td>
+                            <td> 075 753 0000</td>
+                            <td></td>
+                            <td> <a href="https://www.kiertokapula.fi/" target="_blank">www.kiertokapula.fi</a></td>
+                            <td>Kiertokapula OY Vankanlähde 7, 13100 Hämeenlinna </td>
+                        </tr>
+                        <tr>
+                            <td>Kymenlaakson Jäte Oy</td>
+                            <td>(05) 744 3400</td>
+                            <td></td>
+                            <td> <a href="https://www.kymenlaaksonjate.fi/" target="_blank">www.kymenlaaksonjate.fi</a></td>
+                            <td>Kymenlaakson Jäte Oy Ecocycle 5046860 Yellow fabric</td>
+                        </tr>
+                        <tr>
+                            <td>Kiertokapula</td>
+                            <td> 075 753 0000</td>
+                            <td></td>
+                            <td> <a href="" target="_blank"></a></td>
+                            <td>Kiertokapula OY Vankanlähde 7, 13100 Hämeenlinna </td>
                         </tr>
                         </thead>
                         <tbody>

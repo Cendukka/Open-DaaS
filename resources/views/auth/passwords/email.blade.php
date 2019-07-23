5a548460da060fdb9d04d485a23a315d2b109335
meAjit <38285723+meAjit@users.noreply.github.com>
2019-07-23 11:09:52 +0300

    logging in password reset css edit

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
+@extends('layouts.welcomepage')
+@section('title', 'Password reset')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">

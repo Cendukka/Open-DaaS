6ec4ca6e98836bec2459aec195d04834939a54d3
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-07-24 10:16:57 +0300

    Update ewc_codes_table_seeder.php

--- database/seeds/ewc_codes_table_seeder.php
+++ database/seeds/ewc_codes_table_seeder.php
@@ -12,16 +12,39 @@ class ewc_codes_table_seeder extends Seeder
      */
     public function run()
     {
-		$faker = Faker::create();
-		DB::table('ewc_codes')->insert([
-			'ewc_code' => '000000',
-			'description' => 'Default EWC Code'
-		]);
-		foreach (range(1,25) as $index) {
-			DB::table('ewc_codes')->insert([
-				'ewc_code' => sprintf('%02d', rand(1, 20)) . sprintf('%02d', rand(1, 30)) . sprintf('%02d', rand(1, 99)),
-				'description' => $faker->text(100)
-			]);
-		}
+//		$faker = Faker::create();
+		DB::table('ewc_codes')->insert(
+		    [
+		        'ewc_code' => '000000',
+			    'description' => 'Default EWC Code'
+            ],
+            [
+                'ewc_code' => '180104 ',
+                'description' => 'Yritysperäinen tekstiilijäte vastaanotettaessa (ainakin sairaala- ja pesulatekstiilit)'
+            ],
+            [
+                'ewc_code' => '191208 ',
+                'description' => 'Pilaantuneet tekstiilit polttoon'
+            ],
+            [
+                'ewc_code' => '191212 ',
+                'description' => 'Keräykseen kuulumattomat materiaalit polttoon'
+            ],
+            [
+                'ewc_code' => '200110 ',
+                'description' => 'Uudelleenkäytettävät vaatteet (kirpparimyynti yms)'
+            ],
+            [
+                'ewc_code' => '200111 ',
+                'description' => 'Poistotekstiili (kuluttajaperäinen, vastaanotettaessa ja uudelleenkäytettäessä)'
+            ]
+
+        );
+//		foreach (range(1,25) as $index) {
+//			DB::table('ewc_codes')->insert([
+//				'ewc_code' => sprintf('%02d', rand(1, 20)) . sprintf('%02d', rand(1, 30)) . sprintf('%02d', rand(1, 99)),
+//				'description' => $faker->text(100)
+//			]);
+//		}
     }
 }

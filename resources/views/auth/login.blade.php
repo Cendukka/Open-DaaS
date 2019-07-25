8b8c48c4d0b8bc76c0fbde6cee27c6a762261f9a
meAjit <38285723+meAjit@users.noreply.github.com>
2019-07-25 16:29:49 +0300

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,15 +38,6 @@
                             </div>
                         </div>
 
-                        <div class="col-sm-9 form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-
-                     
                         <div class="col-sm-9">
                                 <button type="submit" class="btn btn-primary ">
                                     {{ __('Login') }}

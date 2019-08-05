f45dc80e481543ba3a04ac60f2ebc1e0ab413478
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-08-05 15:33:10 +0300

    Update ewc_codes_table_seeder.php

--- database/seeds/ewc_codes_table_seeder.php
+++ database/seeds/ewc_codes_table_seeder.php
@@ -11,12 +11,17 @@ class ewc_codes_table_seeder extends Seeder {
      */
     public function run() {
         $ewc_codes = [
-            '000000' => 'Default EWC Code',
-            '180104' => 'Yritysperäinen tekstiilijäte vastaanotettaessa (ainakin sairaala- ja pesulatekstiilit)',
+            '00000' => 'Default EWC Code',
+            '040222' => 'Tekstiilituotantojäte yksityiseltä toimittajalta',
+            '040221' => 'Tekstiilituotantojäte yksityiseltä toimittajalta',
+            '150109' => 'Tekstiilipakkaukset, köydet, liinat yms yksityiseltä toimittajalta',
+            '160306' => 'Epäkurantit tuotantoerät tai käyttämättömät tuotteet yksityseltä toimittajalta',
+            '180104' => 'Sairaalalakanat tai muut ei-tartuntavaaralliset terveydenhuollon tekstiilit',
             '191208' => 'Pilaantuneet tekstiilit polttoon',
             '191212' => 'Keräykseen kuulumattomat materiaalit polttoon',
-            '200110' => 'Uudelleenkäytettävät vaatteet (kirpparimyynti yms)',
-            '200111' => 'Poistotekstiili (kuluttajaperäinen, vastaanotettaessa ja uudelleenkäytettäessä)',
+            '200110' => 'Uudelleenkäytettävät vaatteet',
+            '200111' => 'Poistotekstiili uudelleenkäyttöön',
+
         ];
 
         foreach ($ewc_codes as $code => $description) {

ea2f88e9c7ff66e4daec25004add11f28ecf525f
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-08-13 16:20:49 +0300

    deleted (again) un-necessary blades

--- resources/views/includes/macrolocation_name.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="navbar">
-    <div class="navbar-inner">
-{{--        <a id="logo" href="/companies/{{$company->company_id}}">TOIMIPISTE: {{$company->company_name}}</a>--}}
-        @php
-            $no_navbar = isset($no_navbar) ? $no_navbar : false;
-        @endphp
-        @includeWhen(!$no_navbar, 'includes.macrolocation_name_navbar')
-    </div>
-</div>

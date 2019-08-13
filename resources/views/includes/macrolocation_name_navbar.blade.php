49907f1dab62be8363712ba226e54a7f34b2d3b1
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-08-13 14:26:10 +0300

    Deleted un-necessary blades

--- resources/views/includes/macrolocation_name_navbar.blade.php
+++
@@ -1,8 +0,0 @@
- <ul class="nav">
-     <li><a href="{{'/companies/'.$company->company_id.'/warehouse'}}">    Varasto</a></li>
-     <li><a href="{{'/companies/'.$company->company_id.'/receipts'}}">     Saapuneet</a></li>
-     <li><a href="{{'/companies/'.$company->company_id.'/issues'}}">       Lähetetyt</a></li>
-     <li><a href="{{'/companies/'.$company->company_id.'/pre'}}">          Esilajiteltu</a></li>
-     <li><a href="{{'/companies/'.$company->company_id.'/refined'}}">      Hienolajiteltu</a></li>
-
- </ul>

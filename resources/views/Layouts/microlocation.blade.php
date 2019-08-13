491ee1390cb0c8ef495f32eac99f39f204891c79
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-08-13 15:31:46 +0300

    Revert "Deleted un-necessary blades"
    This reverts commit 49907f1dab62be8363712ba226e54a7f34b2d3b1.

---
+++ resources/views/Layouts/microlocation.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        @include('includes.head')
+    </head>
+    <body>
+        <div class="container">
+            <header class="column">
+                @include('includes.microlocation_header')
+            </header>
+        </div>
+    </body>
+</html>
---
+++ resources/views/includes/macrolocation_name_navbar.blade.php
@@ -0,0 +1,8 @@
+ <ul class="nav">
+     <li><a href="{{'/companies/'.$company->company_id.'/warehouse'}}">    Varasto</a></li>
+     <li><a href="{{'/companies/'.$company->company_id.'/receipts'}}">     Saapuneet</a></li>
+     <li><a href="{{'/companies/'.$company->company_id.'/issues'}}">       Lähetetyt</a></li>
+     <li><a href="{{'/companies/'.$company->company_id.'/pre'}}">          Esilajiteltu</a></li>
+     <li><a href="{{'/companies/'.$company->company_id.'/refined'}}">      Hienolajiteltu</a></li>
+
+ </ul>

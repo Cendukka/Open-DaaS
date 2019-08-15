1cec281b38e1e2a0f32cf14c34b9e6bfa083d13d
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-08-15 16:05:09 +0300

    Instructions for formating new organization

--- resources/views/pages/instructions.blade.php
+++ resources/views/pages/instructions.blade.php
@@ -7,6 +7,32 @@
         </div>
         <div class="panel-body row p-4" style="margin-right: 0; margin-left: 0;">
             <div class="col-lg-3">
+                <h6>Uuden organisaation alustus käyttöönottoa varten</h6>
+                <div class="border border-info rounded text-left p-3 text-break">
+                    <dl>
+                        <dt><u>Luodaan toimipisteet</u></dt>
+                            <dd><b>-->Hallinnoi-->Toimipisteet-->Lisää toimipiste</b></dd>
+                            <dd><b>1.Organisaatio:</b> #Automaattinen#</dd>
+                            <dd><b>2.Toimipisteen tyyppi:</b> Valitaan toimipisteelle tyyppi riippuen onko siellä vastaanotto-, lajittelu- ja/tai varastotoimintaa</dd>
+                            <dd><b>3.Toimipisteen nimi:</b> Annetaan, kuvaava nimi esim. Alueen nimi, josta löytyy monta pientä keräyspistettä</dd>
+                            <dd><b>4.Toimipisteen osoitetiedot</b></dd>
+
+                        <dt><u>Luodaan käyttäjät toimipisteisiin</u></dt>
+                            <dd><b>-->Hallinnoi-->Käyttäjät-->Lisää käyttäjä</b></dd>
+                            <dd><b>1.Organisaatio:</b> #Automaattinen#</dd>
+                            <dd><b>2.Käyttäjä tyyppi:</b> Valitaan käyttäjälle tyyppi riippuen onko hän organisaation vastaava vai normaali käyttäjä</dd>
+                            <dd><b>3.Toimipiste:</b> Käyttäjään liitetään toimipiste, missä hän työskentelee</dd>
+                            <dd><b>4.Käyttäjän nimi</b></dd>
+                            <dd><b>5.Käyttäjätunnus</b></dd>
+                            <dd><b>6.Sähköposti</b></dd>
+                            <dd><b>7.Salasana on automaattinen </b></dd>
+
+                        <dt><u>Lisätään tarvittaessa lisää kuntia</u></dt>
+                            <dd>Kunnat lisääntyvät automaattisesti toimipisteiden luomisen yhteydessä</dd>
+                    </dl>
+                </div>
+            </div>
+            <div class="col-lg-3">
                 <h6>Saapuneen lähetyksen kirjaaminen</h6>
                 <div class="border border-info rounded text-left p-3 text-break">
                     <dl>

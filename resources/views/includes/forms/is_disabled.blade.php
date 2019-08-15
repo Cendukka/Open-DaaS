02a474e1c12f388ba437f7071ced7fd196be9481
eemeliranta <50950050+eemeliranta@users.noreply.github.com>
2019-08-15 15:21:35 +0300

    Update is_disabled.blade.php

--- resources/views/includes/forms/is_disabled.blade.php
+++ resources/views/includes/forms/is_disabled.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group row">
     <div class="col-sm-2"></div>
     <div class="col-sm-10">
-        <div class="custom-control custom-checkbox my-1 mr-sm-2">
+        <div style="float:left" class="custom-control custom-checkbox my-1 mr-sm-2">
             <input class="custom-control-input" type="checkbox" id="is_disabled" name="is_disabled" {{isset($is_disabled) ? ($is_disabled > 0 ? 'checked' : '') : ''}}>
             <label class="custom-control-label" for="is_disabled">Poistettu käytöstä</label>
         </div>

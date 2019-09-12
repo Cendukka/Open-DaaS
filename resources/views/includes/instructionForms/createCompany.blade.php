9f225e2905b0f6a302322b835d402a34885a0981
Cenderer <32675164+Cenderer@users.noreply.github.com>
2019-09-12 14:25:25 +0300

    Created include forms

---
+++ resources/views/includes/instructionForms/createCompany.blade.php
@@ -0,0 +1,27 @@
+<div class="col-lg-3">
+    <h6>Uuden organisaation luominen</h6>
+    <br>
+    <div class="border border-info rounded text-left p-3 text-break">
+        <dl>
+            <dt><u>Luodaan toimipisteet</u></dt>
+            <dd><b>-->Hallinnoi-->Toimipisteet-->Lisää toimipiste</b></dd>
+            <dd><b>1.Organisaatio:</b> #Automaattinen#</dd>
+            <dd><b>2.Toimipisteen tyyppi:</b> Valitaan toimipisteelle tyyppi riippuen onko siellä vastaanotto-, lajittelu- ja/tai varastotoimintaa</dd>
+            <dd><b>3.Toimipisteen nimi:</b> Annetaan, kuvaava nimi esim. Alueen nimi, josta löytyy monta pientä keräyspistettä</dd>
+            <dd><b>4.Toimipisteen osoitetiedot</b></dd>
+
+            <dt><u>Luodaan käyttäjät toimipisteisiin</u></dt>
+            <dd><b>-->Hallinnoi-->Käyttäjät-->Lisää käyttäjä</b></dd>
+            <dd><b>1.Organisaatio:</b> #Automaattinen#</dd>
+            <dd><b>2.Käyttäjä tyyppi:</b> Valitaan käyttäjälle tyyppi riippuen onko hän organisaation vastaava vai normaali käyttäjä</dd>
+            <dd><b>3.Toimipiste:</b> Käyttäjään liitetään toimipiste, missä hän työskentelee</dd>
+            <dd><b>4.Käyttäjän nimi</b></dd>
+            <dd><b>5.Käyttäjätunnus</b></dd>
+            <dd><b>6.Sähköposti</b></dd>
+            <dd><b>7.Salasana on automaattinen </b></dd>
+
+            <dt><u>Lisätään tarvittaessa lisää kuntia</u></dt>
+            <dd>Kunnat lisääntyvät automaattisesti toimipisteiden luomisen yhteydessä</dd>
+        </dl>
+    </div>
+</div>
---
+++ resources/views/includes/instructionForms/createRefined.blade.php
@@ -0,0 +1,28 @@
+<div class="col-lg-3">
+    <h6>Hienolajittelukirjauksen kirjaaminen</h6>
+    <br>
+    <div class="border border-info rounded text-left p-3">
+        <dl>
+            <dt>Mistä löytyy?</dt>
+            <dd>-> Materiaalikirjaukset</dd>
+            <dd>-> Lisää hienolajittelu</dd>
+            <dd>-> Täytä lomake</dd>
+            <dd>-> Tarkasta</dd>
+            <dd>-> Paina Lisää-nappulaa</dd>
+
+            <dt>Miten täyttää?</dt>
+            <br>
+            <dd><b>Aika:</b>#Automaattinen# Voit muokata, jos on tarpeellista</dd>
+            <dd><b>Käyttäjä:</b>#Kirjautunut käyttäjä# Managerina mahdollisuus vaihtaa käyttäjää</dd>
+            <dd><b>Toimipiste:</b> Toimipiste missä hienolajittelu tapahtuu</dd>
+            <dd><b>Tekstiilin alkuperä:</b> Valitaan, että hienolajitellaanko saapunutta kirjausta vai esilajittelukirjausta</dd>
+            <dd><b>**Saapuneiden materiaalien kirjaus:</b> Valitaan saapunut-kirjaus</dd>
+            <dd><b>**Esilajittelun kirjaus: Valitaan esilajittelu-kirjaus</b></dd>
+            <dd><b>Tekstiili</b> Valitaan tekstiili, joka syntyy hienolajittelussa</dd>
+            <dd><b>Paino (Kg):</b> Hienolajitellun tekstiilin paino</dd>
+            <dd><b>Lisätietoja:</b> Voidaan kirjata hienolajittelutapahtuman lisätietoja mm. lajittelussa olleet henkilöt</dd>
+            <br>
+            <dd><i>**-Riippuu "Tekstiilin alkuperä:"-kohdan valinnasta </i></dd>
+        </dl>
+    </div>
+</div>
